1fd7ee7b7f02fef276476fe9d4b0875fef3d3661
Martin Wierenga <martinwierenga@live.nl>
2025-06-24 14:47:53 +0200

    Same issue

--- llama-arc-docker/run.sh
+++ llama-arc-docker/run.sh
@@ -3,5 +3,4 @@
 
 # Note: adjust the executable name/path if needed. Often, the provided images entrypoint already
 # points to the server binary so you may simply pass the additional parameters.
-
-exec llama-server -m /models/7B/ggml-model-q4_0.gguf --port 8000 --host 0.0.0.0 -n 512
+ 
