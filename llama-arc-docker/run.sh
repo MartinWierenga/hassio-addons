32fa04ab8b0e842565aa44fe0f6e3367db66d410
Martin Wierenga <martinwierenga@live.nl>
2025-06-24 14:46:38 +0200

    Typo

--- llama-arc-docker/run.sh
+++ llama-arc-docker/run.sh
@@ -3,5 +3,5 @@
 
 # Note: adjust the executable name/path if needed. Often, the provided images entrypoint already
 # points to the server binary so you may simply pass the additional parameters.
-<<<<<<< HEAD
+
 exec llama-server -m /models/7B/ggml-model-q4_0.gguf --port 8000 --host 0.0.0.0 -n 512
