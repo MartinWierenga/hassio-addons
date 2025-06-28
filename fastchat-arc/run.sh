f3568896fd2152b6b0604752bc7d2fdfb344ae84
Martin Wierenga <martinwierenga@live.nl>
2025-06-28 21:26:47 +0200

    test

--- fastchat-arc/run.sh
+++ fastchat-arc/run.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #
-#LOG_PATH=${LOG_PATH:-/logs}
-#mkdir -p "$LOG_PATH"
+LOG_PATH=${LOG_PATH:-/logs}
+mkdir -p "$LOG_PATH"
 #
-#ARGS="--model-path ${MODEL_PATH:-/share/models/naturalfunctions} --max-gpu-memory ${MAX_GPU_MEMORY:-8Gib}"
-#echo "[INFO] Starting FastChat model_worker with args: $ARGS"
+ARGS="--model-path ${MODEL_PATH:-/share/models/naturalfunctions} --max-gpu-memory ${MAX_GPU_MEMORY:-8Gib}"
+echo "[INFO] Starting FastChat model_worker with args: $ARGS"
 #
-#exec python3 -m fastchat.serve.model_worker $ARGS >> "$LOG_PATH/model_worker.log" 2>&1
+python3 -m fastchat.serve.model_worker $ARGS >> "$LOG_PATH/model_worker.log" 2>&1
 #
 #!/usr/bin/env bash
-which python3
-python3 --version
-ls -al /
+#which python3
+#python3 --version
+#ls -al /
