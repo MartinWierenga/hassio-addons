fdb383708d578c9ceb35c7c76b3e9cba8cfcb06f
Martin Wierenga <martinwierenga@live.nl>
2025-06-28 11:18:01 +0200

    TRying with IPEX FastChat

--- llama-arc-docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# Launch the server with the specified model and options
-
-# Note: adjust the executable name/path if needed. Often, the provided images entrypoint already
-# points to the server binary so you may simply pass the additional parameters.
- 
